430bbc9efa15ced20b65e823e2a41c89847139f1
quarkusio <no-reply@quarkus.io>
2021-10-22 00:56:52 +0000

    Initial commit

---
+++ src/test/java/org/yn/app/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.yn.app;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
